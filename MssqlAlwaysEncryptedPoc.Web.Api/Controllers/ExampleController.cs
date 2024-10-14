d3d5cb3bf8e2d17794075e162d71c0a65a304a58
danb <danb@outlook.hu>
2024-10-14 12:47:29 +0200

    API now returns data from the DB

---
+++ MssqlAlwaysEncryptedPoc.Web.Api/Controllers/ExampleController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MssqlAlwaysEncryptedPoc.ExampleDb;
+
+namespace MssqlAlwaysEncryptedPoc.Web.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ExampleController : ControllerBase
+{
+    private readonly ILogger<ExampleController> _logger;
+    private readonly ExampleDbContext _dbContext;
+
+    public ExampleController(ILogger<ExampleController> logger, ExampleDbContext dbContext)
+    {
+        _logger = logger;
+        _dbContext = dbContext;
+    }
+
+    [HttpGet(Name = "GetExamples")]
+    public async Task<IEnumerable<Example>> Get()
+    {
+        return (await _dbContext.Examples.ToListAsync()).Select(e => new Example(e.Id, e.Description));
+    }
+}
---
+++ MssqlAlwaysEncryptedPoc.Web.Api/Example.cs
@@ -0,0 +1,3 @@
+namespace MssqlAlwaysEncryptedPoc.Web.Api;
+
+public record Example(Guid Id, string Description);
